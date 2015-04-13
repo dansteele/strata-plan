f659dcb827df6f6682c57cefd3ff7b8f770dedeb
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-13 22:18:01 +0100

    Added create and show actions to the controller

--- app/controllers/journey_controller.rb
+++ app/controllers/journey_controller.rb
@@ -1,7 +1,19 @@
 class JourneyController < ApplicationController
 
+  def show
+    @journey = Journey.find(params[:id])
+  end
+
   def index
     @journeys = current_traveller.journeys
+  end
+
+  def create
+    if current_traveller.journeys << Journey.create(name: "My new journey")
+      redirect_to journey_path(current_traveller.journeys.last)
+    else
+      render text: "Unable to create journey"
+    end
   end
 
 end
