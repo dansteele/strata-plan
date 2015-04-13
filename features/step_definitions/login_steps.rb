eab166681a6b66412d2481934108527e565f8423
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-13 13:37:59 +0100

    Installed devise

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,31 @@
+Given(/^they click on "(.*?)"$/) do |target|
+  click_on target
+end
+
+Given(/^they fill in the registration form$/) do
+  page.fill_in 'Email', :with => "Bob" #faker
+end
+
+When(/^they press submit$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^there should be (\d+) users in the database$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^a visitor has an account$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^they click on "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^they fill in the login form$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^they should be logged in$/) do
+  pending # express the regexp above with the code you wish you had
+end
