3036624bc53593b738c17e53aa4cde947d05cb80
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-13 21:49:59 +0100

    Wrote test for associations

--- spec/models/traveller_spec.rb
+++ spec/models/traveller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Traveller, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "travellers have journeys" do
+
+    before do
+      @traveller = FactoryGirl::create(:traveller)
+      @traveller.journeys.create(name: "Trip to Spain")
+      @traveller.journeys << Journey.new(name: "Honeymoon")
+    end
+
+    it "should have multiple journeys" do
+      expect(@traveller.journeys.count).to eq 2
+      expect(@traveller.journeys.second.name).to eq "Honeymoon" 
+    end
+
+  end
+
+
 end
