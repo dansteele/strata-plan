761939316fc86db1be43cb54030f7f6e9646cb7a
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-14 11:02:43 +0100

    Spelling

--- spec/models/waypoint_spec.rb
+++ spec/models/waypoint_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Waypoint, type: :model do
   describe "name generation" do
 
     it "should save a name" do
-      @wayp = Waypoint.create(longitude:51.5072, lat: 0.1275)
+      @wayp = Waypoint.create(longitude:51.5072, latitude: 0.1275)
       expect(@wayp.name).to_not eq nil
     end
 
