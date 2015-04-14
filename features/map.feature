f2e00bb08c62b6ab30dee56801975e7106afbc70
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-14 14:31:44 +0100

    Added more detail to map test

--- features/map.feature
+++ features/map.feature
@@ -5,5 +5,6 @@ Feature: Map
       And a traveller has signed up
 
     Scenario: View the map
-      Given a traveller is on their first journey
+      Given a traveller starts a new jourmey
+        And they are looking at the new journey
       Then they should see a map
--- features/step_definitions/map_steps.rb
+++ features/step_definitions/map_steps.rb
@@ -1,5 +1,10 @@
-Given(/^a traveller is on their first journey$/) do
-  pending # express the regexp above with the code you wish you had
+Given(/^a traveller starts a new jourmey$/) do
+  click_on "My journeys"
+end
+
+Given(/^they are looking at the new journey$/) do
+  click_on "New journey"
+  expect(page.has_content? "My new journey").to be true
 end
 
 Then(/^they should see a map$/) do
