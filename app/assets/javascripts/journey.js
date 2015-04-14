4a09cd60c96b5552b7c04634a34e6747559037d5
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-14 10:04:57 +0100

    Added journey map

---
+++ app/assets/javascripts/journey.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  map = new JourneyMap(-34.397, 150.644, 9)
+  map.drawMap('#map-canvas')
+});
+
+function JourneyMap(lat, lng, zoom_level) {
+  this.center = new google.maps.LatLng(lat, lng);
+  this.zoom_level = zoom_level;
+}
+
+JourneyMap.prototype.fitView = function(latLngList) {
+  var bounds = new google.maps.LatLngBounds();
+  var LtLgLen = latLngList.length
+  for (var i = 0; i < LtLgLen; i++) {
+    bounds.extend(latLngList[i]);
+  }
+  map.map.fitBounds(bounds);
+}
+
+JourneyMap.prototype.drawMap = function(jquerySelector) {
+  var map_options = {
+    center: this.center,
+    zoom: this.zoom_level
+  }
+  var map_canvas = $(jquerySelector)[0]
+  this.map = new google.maps.Map(map_canvas, map_options);
+}
+
+
+JourneyMap.prototype.addMarker = function(title, lat, lng, id) {
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(lat, lng),
+    map: this.map,
+    title: title
+  })
+  google.maps.event.addListener(marker, 'click', function(){
+    newUrl = String(window.location).replace(/#.+/g, "")
+    window.location = (newUrl+ id)
+  })
+}
