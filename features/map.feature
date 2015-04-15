9776991ec433bbfb3b6946ba193868e1d4bdec68
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-15 10:16:24 +0100

    Wrote test for adding a waypoint

--- features/map.feature
+++ features/map.feature
@@ -5,6 +5,12 @@ Feature: Map
       And a traveller has signed up
 
     Scenario: View the map
-      Given a traveller starts a new jourmey
+      Given a traveller starts a new journey
         And they are looking at the new journey
       Then they should see a map
+
+    @javascript
+    Scenario: Add a waypoint
+      Given a traveller is looking at a map
+      When they double click on the map
+      Then they should have a new waypoint
--- features/step_definitions/map_steps.rb
+++ features/step_definitions/map_steps.rb
@@ -1,4 +1,4 @@
-Given(/^a traveller starts a new jourmey$/) do
+Given(/^a traveller starts a new journey$/) do
   click_on "My journeys"
 end
 
@@ -7,6 +7,22 @@ Given(/^they are looking at the new journey$/) do
   expect(page.has_content? "My new journey").to be true
 end
 
+Given(/^a traveller is looking at a map$/) do
+  steps %Q{
+    Given a traveller starts a new journey
+    And they are looking at the new journey
+    Then they should see a map
+  }
+end
+
+When(/^they double click on the map$/) do
+  2.times {find("#map-canvas").click}
+end
+
 Then(/^they should see a map$/) do
   expect(page.has_content? "map-canvas")
+end
+
+Then(/^they should have a new waypoint$/) do
+  expect(page.all('.waypoint').count).to be 1
 end
