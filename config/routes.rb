11a7d89386d63c1dc83816b5b1bc8755a2594bd1
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-15 14:02:55 +0100

    Added route for waypoints/create

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :travellers
   
   resources :journey, only: [:index, :create, :show]
+  resources :waypoints, only: [:create]
 
   root to: "welcome#index"
 end
