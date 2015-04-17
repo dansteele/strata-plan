3c2497cf67de94e4a083afeffbe64de32f21a45d
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-17 20:17:30 +0100

    Removing a waypoint updated map.

--- app/assets/javascripts/delete_waypoint.js
+++ app/assets/javascripts/delete_waypoint.js
@@ -10,8 +10,10 @@ function addDeleteListener() {
       method: "DELETE",
       url: "http://localhost:3000/waypoints/" + _this.id, // TODO: Make dynamic 
       success: function(res) {
-        $(this.parent.parentElement).hide(300);
-        
+        $(this.parent.parentElement).hide(300, function() {
+          $(this).remove()
+          map.drawMap("#map-canvas")
+        });
       }
     })
   })
