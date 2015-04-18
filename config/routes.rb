fc4af9cc68174abbc23798ac4d5848f5c1bf7aa5
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-18 14:35:29 +0100

    Added controller to select area of system

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   resources :journey, only: [:index, :create, :show, :new]
   resources :waypoints, only: [:create, :destroy]
+  resources :trip, only: [:index]
+  resources :flights, only: [:index]
 
   root to: "welcome#index"
 end
