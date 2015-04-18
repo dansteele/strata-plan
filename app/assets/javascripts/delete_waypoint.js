155154918103523c4367616d5035f7a640b2f285
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-18 11:24:23 +0100

    Changed AJAX target to localhost

--- app/assets/javascripts/delete_waypoint.js
+++ app/assets/javascripts/delete_waypoint.js
@@ -8,7 +8,7 @@ function addDeleteListener() {
     $.ajax({
       parent: _this,
       method: "DELETE",
-      url: "http://strata-plan.herokuapp.com/waypoints/" + _this.id, // TODO: Make dynamic 
+      url: "http://localhost:3000/waypoints/" + _this.id, // TODO: Make dynamic 
       success: function(res) {
         $(this.parent.parentElement).hide(300, function() {
           $(this).remove()
