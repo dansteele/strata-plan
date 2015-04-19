ca23f212b1924150dce2a8b13c4ce75d825f4fdd
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-19 12:06:33 +0100

    Added push observer notification (push)

--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -1,6 +1,11 @@
 class Waypoint < ActiveRecord::Base
+
   belongs_to :journey
   acts_as_list scope: :journey
+
+  after_create do
+    notify_observers_of_create
+  end
 
   reverse_geocoded_by :longitude, :latitude do |obj,results|
     if geo = results.first
@@ -9,5 +14,14 @@ class Waypoint < ActiveRecord::Base
   end
 
   after_validation :reverse_geocode
+
+  private
+  def find_nearest_airport
+    "Heathrow"
+  end
+
+  def notify_observers_of_create
+    Flight.notify_waypoint_create(self)
+  end
 
 end
