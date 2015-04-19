216fab5c2326ba5ad5a39b6b25ef7f31ce5e92eb
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-19 14:46:41 +0100

    Added test for nearest airport

--- spec/models/flight_plan_spec.rb
+++ spec/models/flight_plan_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe FlightPlan, type: :model do
-  describe "test associations" do
+  describe "relationships" do
     before do
       @flight = Flight.create(name: "test")
       @flight.update(flight_plan: FlightPlan.create(start: "-0.5,51.123154"))
+      @flight.reload
     end
 
 
     it "should be part of a flight" do
       expect(@flight.flight_plan.start).to_not be nil
     end
+
+    it "should find the nearest airport" do
+      expect(@flight.flight_plan.start_airport).to eq "Heathrow"
+    end
+
+
   end
 end
