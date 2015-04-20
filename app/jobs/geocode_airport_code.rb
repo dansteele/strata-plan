3574d14a7af7343cc60ee421e9458d6aa3600548
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-20 14:14:07 +0100

    Removed unnecessary var assignment

--- app/jobs/geocode_airport_code.rb
+++ app/jobs/geocode_airport_code.rb
@@ -2,7 +2,6 @@ class GeocodeAirportCode
   @queue = :geocode_airport
 
   def work flight_plan
-    response = HTTParty.get("http://www.airport-data.com/api/ap_info.json?iata=LHR")
-    binding.pry
+    HTTParty.get("http://www.airport-data.com/api/ap_info.json?iata=LHR")
   end
 end
