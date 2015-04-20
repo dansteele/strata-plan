a9471c626500b41dfc06edcc53fcd8acc7a030ad
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-20 17:23:11 +0100

    Click on submit button

--- features/step_definitions/flight_steps.rb
+++ features/step_definitions/flight_steps.rb
@@ -6,6 +6,7 @@ When(/^create a flight for (\d+) people from "(.*?)" to "(.*?)"$/) do |people, s
   fill_in "Start country", with: start_country
   fill_in "End city", with: finish_city
   fill_in "End country", with: finish_country
+  click_on "Create Flight"
 end
 
 Then(/^they should see "(.*?)" and "(.*?)"$/) do |iata1, iata2|
