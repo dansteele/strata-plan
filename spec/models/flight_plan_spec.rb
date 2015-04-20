f11bdd74fa71c7e1aa145afe96e964e9836d1b0e
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-20 20:29:00 +0100

    Updated Rspec test

--- spec/models/flight_plan_spec.rb
+++ spec/models/flight_plan_spec.rb
@@ -4,16 +4,19 @@ RSpec.describe FlightPlan, type: :model do
   describe "relationships" do
     before do
       @flight = Flight.create(name: "test")
-      @flight.update(flight_plan: FlightPlan.create(start: "-0.5,51.123154"))
+      Airport.create(name: "Heathrow", longitude: -0.5, latitude: 51.123)
+      Airport.create(name: "Somewhere else", longitude: 23, latitude: 32)
+      @flight.update(flight_plan: FlightPlan.create(start_city: "Hayes", start_country: "UK",
+      end_city: "London", end_country: "UK"   ))
       @flight.reload
     end
 
     it "should be part of a flight" do
-      expect(@flight.flight_plan.start).to_not be nil
+      expect(@flight.flight_plan).to_not be nil
     end
 
     it "should find the nearest airport" do
-      expect(@flight.flight_plan.start_airport).to eq "Heathrow"
+      expect(@flight.flight_plan.start_airport.name).to eq "Heathrow"
     end
 
   end
