8f2fa32e4c1d36897043e5089011e1d63f33ba9d
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-20 10:00:00 +0100

    Airport module working, but will try local calculation of nearest airport

--- app/models/flight_plan.rb
+++ app/models/flight_plan.rb
@@ -1,2 +1,12 @@
 class FlightPlan < ActiveRecord::Base
+
+  # after_create do
+  #   Resque.new << GeocodeAirportCode.new
+  # end
+
+  before_save do
+    # self.start = Airport::lng_lat_of_airport self.start_airport
+    # self.end = Airport::lng_lat_of_airport self.end_airport
+  end
+
 end
