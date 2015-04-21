bc2f85ba3b3a2b1cb02ed9f008422a41b91f7386
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-21 11:16:06 +0100

    Stubbed out tests

--- spec/models/flight_plan_spec.rb
+++ spec/models/flight_plan_spec.rb
@@ -3,11 +3,20 @@ require 'rails_helper'
 RSpec.describe FlightPlan, type: :model do
   describe "relationships" do
     before do
+      # WebMock.allow_net_connect!
+      WebMock::stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=Hayes,%20UK&key=AIzaSyBwGmEeg9OKIjaWiPE5-GnPR75Y1KXm14g&language=en&sensor=false").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => File.read("spec/stubs/flight_start_geocode.json"), :headers => {})
+
+      WebMock::stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=Dusseldorf,%20Germany&key=AIzaSyBwGmEeg9OKIjaWiPE5-GnPR75Y1KXm14g&language=en&sensor=false").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => File.read("spec/stubs/flight_end_geocode.json"), :headers => {})
+
       @flight = Flight.create(name: "test")
       Airport.create(name: "Heathrow", longitude: -0.5, latitude: 51.123)
-      Airport.create(name: "Somewhere else", longitude: 23, latitude: 32)
+      Airport.create(name: "Dusseldorf", longitude: 6.766667, latitude: 51.289444)
       @flight.update(flight_plan: FlightPlan.create(start_city: "Hayes", start_country: "UK",
-      end_city: "London", end_country: "UK"   ))
+      end_city: "Dusseldorf", end_country: "Germany"   ))
       @flight.reload
     end
 
@@ -17,6 +26,13 @@ RSpec.describe FlightPlan, type: :model do
 
     it "should find the nearest airport" do
       expect(@flight.flight_plan.start_airport.name).to eq "Heathrow"
+      expect(@flight.flight_plan.end_airport.name).to eq "Dusseldorf"
+    end
+
+    it "should find the distance between the airports" do
+      expect(
+        @flight.flight_plan.start_airport.distance_to @flight.flight_plan.end_airport
+      ).to be_within(10).of(310)
     end
 
   end
--- spec/models/waypoint_spec.rb
+++ spec/models/waypoint_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Waypoint, type: :model do
 
     it "should save a name" do
       @journey = Journey.create!(name: "test")
+      WebMock::stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyBwGmEeg9OKIjaWiPE5-GnPR75Y1KXm14g&language=en&latlng=51.5072,0.1275&sensor=false").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => File.read("spec/stubs/waypoint_geocode.json"), :headers => {})
       @journey.waypoints << Waypoint.new(longitude:51.5072, latitude: 0.1275)
       @journey.save!
 
