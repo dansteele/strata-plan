400d10659a930f73b1b170a33dbb6696eed47dca
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-21 17:44:07 +0100

    Removed superfluous add_javascript method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
-  def add_javascript(*files)
-    content_for(:head) { javascript_include_tag(*files) }
-  end
+  # def add_javascript(*files)
+  #   content_for(:head) { javascript_include_tag(*files) }
+  # end
 
 end
