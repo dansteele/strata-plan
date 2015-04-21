414570000cad590f965806e072faef6e826985dd
Daniel Steele <danielsteele@hotmail.co.uk>
2015-04-21 11:39:27 +0100

    Created MockShortener module

--- spec/models/flight_plan_spec.rb
+++ spec/models/flight_plan_spec.rb
@@ -4,13 +4,9 @@ RSpec.describe FlightPlan, type: :model do
   describe "relationships" do
     before do
       # WebMock.allow_net_connect!
-      WebMock::stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=Hayes,%20UK&key=AIzaSyBwGmEeg9OKIjaWiPE5-GnPR75Y1KXm14g&language=en&sensor=false").
-         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-         to_return(:status => 200, :body => File.read("spec/stubs/flight_start_geocode.json"), :headers => {})
-
-      WebMock::stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=Dusseldorf,%20Germany&key=AIzaSyBwGmEeg9OKIjaWiPE5-GnPR75Y1KXm14g&language=en&sensor=false").
-         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-         to_return(:status => 200, :body => File.read("spec/stubs/flight_end_geocode.json"), :headers => {})
+      require "stubs/mock_shortener_module.rb"
+      MockShortener::mock "Hayes, UK", "flight_start_geocode"
+      MockShortener::mock "Dusseldorf, Germany", "flight_end_geocode"
 
       @flight = Flight.create(name: "test")
       Airport.create(name: "Heathrow", longitude: -0.5, latitude: 51.123)
---
+++ spec/stubs/mock_shortener_module.rb
@@ -0,0 +1,9 @@
+module MockShortener
+
+  def self.mock query, file_name
+    WebMock::stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=#{CGI::escape(query)}&key=AIzaSyBwGmEeg9OKIjaWiPE5-GnPR75Y1KXm14g&language=en&sensor=false").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+         to_return(:status => 200, :body => File.read("spec/stubs/#{file_name}.json"), :headers => {})
+  end
+
+end
